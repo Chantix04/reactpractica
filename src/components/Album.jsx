9794317ac49ec94f431315dd9d3f7a49972874df
Chantix04 <gstnrcld@gmail.com>
2022-10-21 14:14:42 -0300

    primer commit

---
+++ src/components/Album.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ContainerAlbum from './ContainerAlbum'
+
+export default function Album() {
+  return (
+    <ContainerAlbum/>
+  )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Section from './Section'
+import ContainerAlbum from './ContainerAlbum'
+
+export default function Main() {
+  return (
+    <main className='Main'>
+        <Section/>
+        <ContainerAlbum/>
+    </main>
+  )
+}
